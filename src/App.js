676764a27bd8df5087a87085dd8205c3c77468a9
Pawel <paweljozwiak1986@gmail.com>
2019-02-09 14:07:26 +0100

    Passin chidren to paragraph component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Paragraph from './components/Paragraph'
+import ReactLink from './components/ReactLink'
 
 
 class App extends Component {
@@ -10,15 +11,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Paragraph />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Paragraph>
+            Edit <code>src/App.js</code> and save to reload.
+          </Paragraph>
+          <Paragraph>
+            Another paragraph.
+          </Paragraph>
+          <ReactLink />
         </header>
       </div>
     );
--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -4,7 +4,7 @@ class Paragraph extends React.Component {
   render() {
     return (
       <p>
-        Edit <code>src/App.js</code> and save to reload.
+        {this.props.children}
       </p>
     )
   }
