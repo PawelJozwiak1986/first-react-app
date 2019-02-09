83ac9463d7a093999751928353f590e88eb0e073
Pawel <paweljozwiak1986@gmail.com>
2019-02-09 13:57:21 +0100

    App initiation and first components

---
+++ src/components/Paragraph.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Paragraph extends React.Component {
+  render() {
+    return (
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    )
+  }
+}
+
+export default Paragraph
