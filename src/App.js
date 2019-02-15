67b78bbc5d9ed639f4f7ec9995339283bec6d5d1
Pawel <paweljozwiak1986@gmail.com>
2019-02-15 12:27:19 +0100

    displaying beaver with styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Paragraph from './components/Paragraph'
-import ReactLink from './components/ReactLink'
+// import Paragraph from './components/Paragraph'
+// import ReactLink from './components/ReactLink'
+import Bobr from './components/Bobr'
+
 
 
 class App extends Component {
   state = {
-    isBold: false,
+    visibility: false,
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Paragraph bold={this.state.isBold}>
-            Edit <code>src/App.js</code> and save to reload.
-          </Paragraph>
-          <Paragraph bold={this.state.isBold}>
-            Another bold paragraph.
-          </Paragraph>
-          <ReactLink url="https://reactjs.org" label="Learn React" />
-          <button onClick={() => this.setState({ isBold: true })}>Pogrub</button>
-          <button onClick={() => this.setState({ isBold: false })}>Odgrub</button>
+          <button onClick={() => this.setState({ visibility: true })}>Bóbr</button>
+          <button onClick={() => this.setState({ visibility: false })}>OdBóbr</button>
+
+          {this.state.visibility && <Bobr />}
         </header>
       </div>
     );
---
+++ src/components/Bobr.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class Bobr extends React.Component {
+  render() {
+    return (
+      <div>
+      <br/>
+      <img
+        src="http://www.drapiezniki.pl/Photos/bobr-na-trawie.jpg"
+        alt="gugr"
+      />
+      <br/>
+      <br/>
+     
+      <span>Siekacze i dziwny ogon</span>
+    </div>
+    )
+  }
+}
+
+export default Bobr
+
+  
